ca63a5975d6293f97bb0de11ba257fa0e4ad429a
JulStrat <ioulianos.kakoulidis@hotmail.com>
2017-12-31 15:31:35 +0200

    Update test_unionfind.py

--- graphtheory/structures/tests/test_unionfind.py
+++ graphtheory/structures/tests/test_unionfind.py
@@ -23,6 +23,19 @@ class TestUnionFind(unittest.TestCase):
         self.assertTrue(algorithm.find(0) == algorithm.find(6))
         self.assertTrue(algorithm.find(1) == algorithm.find(9))
 
+    def test_unionfind_init(self):
+        algorithm = UnionFind(range(10))
+        pairs = [(0, 1), (2, 3), (4, 5), (6, 7), (8, 9), (3, 1)]
+        for a, b in pairs:
+            algorithm.union(a, b)
+        self.assertTrue(algorithm.find(1) == algorithm.find(2))
+        self.assertTrue(algorithm.find(7) != algorithm.find(9))
+        pairs = [(7, 5), (3, 7), (8, 7)]
+        for a, b in pairs:
+            algorithm.union(a, b)
+        self.assertTrue(algorithm.find(0) == algorithm.find(6))
+        self.assertTrue(algorithm.find(1) == algorithm.find(9))
+
     def tearDown(self): pass
 
 if __name__ == "__main__":
